27df64b3b7779a9fc23e58e161b90ffe082805b0
MichalSalek <contact@michalsalek.com>
2025-09-19 13:47:36 +0200

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,5 @@
-import {Metadata} from 'next';
 import {Head, Html, Main, NextScript} from 'next/document'
 
-
-export const metadata: Metadata = {
-  title: "PWA NextJS",
-  description: "It's a simple progressive web application made with NextJS",
-  generator: "Next.js",
-  manifest: "/manifest.json",
-  keywords: ["nextjs"],
-  authors: [
-    {
-      name: "asd",
-      url: "https://adres.pl/",
-    },
-  ],
-  icons: [],
-};
 
 export default function Document() {
   return (
